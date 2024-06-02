7fabd9dc1895028b31b43ffd7c5c82c5c2b49cf7
Hardik Singhal <hardiksinghal02@gmail.com>
2024-06-02 18:56:36 +0530

    Added map for temperature change

--- functions/src/controllers/ac-controller.js
+++ functions/src/controllers/ac-controller.js
@@ -8,26 +8,40 @@ const headers = {
 }
 
 const map = {
-    "ON" : 1,
-    "OFF" : 2
+    "ON" : 25,
+    "OFF" : 0,
+    "16" : 16,
+    "17" : 17,
+    "18" : 18,
+    "19" : 19,
+    "20" : 20,
+    "21" : 21,
+    "22" : 22,
+    "23" : 23,
+    "24" : 24,
+    "25" : 25,
+    "26" : 26,
+    "27" : 27,
+    "28" : 28,
+    "29" : 29,
+    "30" : 30,
 }
 
 
-const sendSignal = (input) => {
+const sendSignal = async (input) => {
     var signal = map[input.toUpperCase()];
     console.log(signal);
     payload = {
         'value' : signal
     }
-    return restController.sendPostRequest(ADAFRUIT_HOST, ADAFRUIT_WEBHOOK_PATH, payload, headers)
-    
-    .then(resp => {
-        if(resp.statusCode == 200 || resp.statusCode == 204) {
-            return resp.responseString
-        }
-        console.error(resp)
-        return "resp.statusCode";
-    })
+    const resp = await restController.sendPostRequest(ADAFRUIT_HOST, ADAFRUIT_WEBHOOK_PATH, payload, headers)
+    if (resp.statusCode == 200 || resp.statusCode == 204) {
+
+        
+        return resp.responseString
+    }
+    console.error(resp)
+    return "resp.statusCode"
 }
 
 
