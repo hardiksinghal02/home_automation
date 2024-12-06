995d797a159d587f78691298b68ea772fadf6f60
Hardik Singhal <hardiksinghal02@gmail.com>
2024-12-07 01:47:22 +0530

    fix

--- chat_bot/app.py
+++ chat_bot/app.py
@@ -42,10 +42,4 @@ def get_gemini_response(prompt):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8502)
-
-# if __name__ == "__main__":
-#     while True:
-#         prompt = input("You : ")
-#         resp = get_gemini_response(prompt)
-#         print(f"Gemini : {resp}")
+    app.run(debug=True, host='0.0.0.0', port=8502)
